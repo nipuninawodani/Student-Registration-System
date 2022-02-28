46c9496173278bc1de1b674197f4b74fae732845
nipuninawodani <nipuninawodani999@gmail.com>
2022-02-28 08:50:58 +0530

    initial commit

---
+++ backend/backend/src/main/java/com/kelaniya/uni/backend/StudentController.java
@@ -0,0 +1,13 @@
+package com.kelaniya.uni.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/student")
+    public String getStudent(){
+        return "Nimal,17,Colombo";
+    }
+}
